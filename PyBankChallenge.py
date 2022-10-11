3d5c5e371f4ca65ff6b8e04addd589b737874e64
LibertyHeise <109040678+LibertyHeise@users.noreply.github.com>
2022-10-11 13:55:34 -0500

    New changes to the csv reader and header

--- PyBankChallenge.py
+++ PyBankChallenge.py
@@ -2,25 +2,30 @@ import os
 import csv
 
 # path to collect data from the Resources folder
-budget_data = os.path.join('Resources', 'budget_data.csv')
+cvspath = os.path.join('Resources', 'budget_data.csv')
 
-with open(budget_data,'r') as csvfile:
+totalmonths = []
 
-    csvreader = csv.reader(csvfile) #, delimiter=',')
-    header = next(csvreader)
+with open(cvspath) as csvfile:
+
+    csvreader = csv.reader(csvfile)
+    print(cvsreader)
+
     csv_header = next(csvreader)
+    print(f"CSV Header: {csv_header}")
 
-print(f"CSV Header: {csv_header}")
-
-for row in csvreader:
-
-    print(row[0],row[1])
-    print('\n',"-" * 75,'\n')
+    for row in csvreader:
+        totalmonths.append(row[0])
+        #print(row[0],row[1])
+        #print('\n',"-" * 75,'\n')
 
 # total number of months included in the dataset
 #def print_months(month):
 # net total amount of "profit/losses" over the entire period
 #total_months = str(date[0])
+#i + with a four loop
+#only have profit change for 85 of the months
+
 # changes in "profit/losses" over the entire period, including average of those changes
 #profit_loss = 
 #net_total = [total_months]
