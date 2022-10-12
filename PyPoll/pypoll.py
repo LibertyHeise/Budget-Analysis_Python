cdf0ad45bb899aaa141a8e6248170074fdf16efb
LibertyHeise <109040678+LibertyHeise@users.noreply.github.com>
2022-10-12 10:34:34 -0500

    Outputs updated

--- PyPoll/pypoll.py
+++ PyPoll/pypoll.py
@@ -9,5 +9,35 @@ election_data_csv = os.path.join(Resources.election_data.csv)
 text_path = "election_output.txt"
 
 # Set variables
+totalvotes = 0
+popularvote = 0
 
+
+# The total number of votes cast
+
+# A complete list of candidates who received votes
+
+# The percentage of votes each candidate won
+
+# The total number of votes each candidate won
+
+# The winner of the election based on popular vote
+
+# Format for outputs
+
+Output = (
+    f"Election Results \n"
+    f"----------------------------\n"
+    f"\n"
+    f"Total Votes: {totalvotes}\n"
+    f"----------------------------\n"
+    f"\n"    
+
+
+
+    f"----------------------------\n"
+    f"\n"
+    f"Winner: {popularvote}")
+    f"\n"
+    f"----------------------------\n"
 
