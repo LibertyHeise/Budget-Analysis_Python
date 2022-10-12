f68366c2e04ec7e2b73e29d1e8a37c64ac7f4986
LibertyHeise <109040678+LibertyHeise@users.noreply.github.com>
2022-10-12 10:59:30 -0500

    updates

--- PyPoll/pypoll.py
+++ PyPoll/pypoll.py
@@ -3,10 +3,10 @@ import os
 import csv
 
 # Set path for file
-election_data_csv = os.path.join(Resources.election_data.csv)
+election_data_csv = os.path.join('Resources', 'election_data.csv')
 
 # Set the output of the text file
-text_path = "election_output.txt"
+text_path = os.path.join('analysis', 'election_output.txt')
 
 # Set variables
 totalvotes = 0
@@ -38,6 +38,7 @@ Output = (
     f"----------------------------\n"
     f"\n"
     f"Winner: {popularvote}")
-    f"\n"
-    f"----------------------------\n"
+f"----------------------------\n"
+')'
 
+print(Output)
