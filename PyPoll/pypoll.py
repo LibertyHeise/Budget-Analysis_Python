7e7b59a76be53de9b4740b20b5329251edec763f
LibertyHeise <109040678+LibertyHeise@users.noreply.github.com>
2022-10-12 10:07:14 -0500

    First update to PyPoll

---
+++ PyPoll/pypoll.py
@@ -0,0 +1,13 @@
+# Import files
+import os
+import csv
+
+# Set path for file
+election_data_csv = os.path.join(Resources.election_data.csv)
+
+# Set the output of the text file
+text_path = "election_output.txt"
+
+# Set variables
+
+
