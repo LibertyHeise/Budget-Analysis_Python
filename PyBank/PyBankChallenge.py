e9a0f42caa94b737871776840017ce419c93b64c
LibertyHeise <109040678+LibertyHeise@users.noreply.github.com>
2022-10-12 09:56:40 -0500

    next to final changes

--- PyBank/PyBankChallenge.py
+++ PyBank/PyBankChallenge.py
@@ -30,17 +30,28 @@ with open(csvpath) as csvfile:
         # Count total months
         totalmonths += 1
 
-        # Average change between months
-        net_total += int(row[1])
+        # # Average change between months
+        # net_total += int(row[1])
 
-        # Average change of total between months over the entire period
+        # # Average change of total between months over the entire period
         average_change = float(row[0]-previous_total)
         previous_total = float(row [1])
         total_change_list = total_change_list + [average_change]
         monthchange = [monthchange] + [row[1]]
 
+        # Greatest increase in profits (date + amount)
+        if average_change>greatest_in[1]:
+            greatest_in[1]= average_change
+            greatest_in[0]= row[0]
+
+        # Greatest decrease in profits (date + amount)
+        if average_change<greatest_dec[1]:
+            greatest_dec[1] = average_change
+            greatest_dec[0] = row[0]
+    average_change = sum(total_change_list)/len(total_change_list)
 
 
+# Format for outputs
 Output = (
     f"Financial Analysis \n"
     f"----------------------------\n"
@@ -50,8 +61,6 @@ Output = (
     f"Average Change: {average_change}\n"
     f"Greatest Increase in Profits: {greatest_in}\n"
     f"Greatest Decrease in Profits: {greatest_dec}\n"
-    
-
 
 ) 
 print(Output)
@@ -59,9 +68,6 @@ print(Output)
 # save results to Resources folder
 with open(reportpath, "w") as txtfile:
     txtfile.write(Output)
-
-# #greatest decrease in profits (date/amount) over the entire period
-
 
 
 
