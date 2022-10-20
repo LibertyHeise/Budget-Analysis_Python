a563ec38913c0cac0f5fccf6d7de09a43fe278d4
LibertyHeise <109040678+LibertyHeise@users.noreply.github.com>
2022-10-20 09:45:27 -0700

    updates

--- PyPoll/pypoll.py
+++ PyPoll/pypoll.py
@@ -12,55 +12,55 @@ text_path = os.path.join('analysis', 'election_output.txt')
 with open(csv_path) as csv_file:
     csv_reader = csv.reader(csv_file, delimiter = ",")
     csv_header = next(csv_file)
-    print(f"Header:{csv_header}")
-    output.write(f"Header:{csv_header}\n")
-
+    #print(f"Header:{csv_header}")
+  
 # Set variables
-votes = 0
-total_votes_cast = 0
-candidate_s = []
-candidate_wins = {}
-winning_can = ""
-winning_count = 0
+    votes = 0
+    total_votes_cast = 0
+    candidate_s = []
+    candidate_wins = {}
+    winning_can = ""
+    winning_count = 0
+    totalvotes = 0
+    popularvote = 0
+
+    #Total votes cast
+    for row in csv_reader:
+        votes +=1
+
+    # A complete list of candidates who received votes and the total number of votes won by candidate
+        if row[2] not in candidate_s:
+            candidate_s.append(row[2])
+            candidate_wins[row[2]]=1
+           
+
+    # The percentage of votes each candidate won
+
+    # The total number of votes each candidate won
+
+    # The winner of the election based on popular vote
+
+    # Format for outputs
+
+    Output = (
+        f"Election Results \n"
+        f"----------------------------\n"
+        f"\n"
+        f"Total Votes: {totalvotes}\n"
+        f"----------------------------\n"
+        f"\n"    
 
 
-#Total votes cast
-for row in csv_reader:
-    votes +=1
 
-# A complete list of candidates who received votes and the total number of votes won by candidate
-    if row[2] not in candidate_s:
-        candidate_s.append(row[2])
-        numberwon [row[2]] = 0
-    numberwon [row[2]]+= 1
-
-# The percentage of votes each candidate won
-
-# The total number of votes each candidate won
-
-# The winner of the election based on popular vote
-
-# Format for outputs
-
-Output = (
-    f"Election Results \n"
+        f"----------------------------\n"
+        f"\n"
+        f"Winner: {popularvote}")
     f"----------------------------\n"
-    f"\n"
-    f"Total Votes: {totalvotes}\n"
-    f"----------------------------\n"
-    f"\n"    
 
 
 
-    f"----------------------------\n"
-    f"\n"
-    f"Winner: {popularvote}")
-f"----------------------------\n"
+    print(Output)
 
-
-
-print(Output)
-
-# save results to analysis folder
-with open(text_path, "w") as txtfile:
-    txtfile.write(Output)
+    # save results to analysis folder
+    with open(text_path, "w") as txtfile:
+        txtfile.write(Output)
